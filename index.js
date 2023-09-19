140d2c03b2ecbd2ab0594800192af473fa2be8e0
Ahnaf Gibran <ahnafgibran@gmail.com>
2023-09-20 01:43:13 +0700

    finish

--- index.js
+++ index.js
@@ -24,6 +24,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, res) => {
+  let date;
+  if (req.params.date) {
+      date = new Date(req.params.date);
+  } else {
+      date = new Date();
+  }
+  if (isNaN(date.getTime())) {
+      res.status(400).json({error: "Invalid Date"});
+  } else {
+      res.json({unix: date.getTime(), utc: date.toUTCString()});
+  }
+});
+
+
 
 
 // listen for requests :)
